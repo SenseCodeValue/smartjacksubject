4af58442cbaa5b8faee40d308c989e057ee0ea34
SCV <sencecdoevalue@gmail.com>
2020-06-10 17:53:40 +0900

    :hammer: 구조 개선

--- algorithm/algo.js
+++ algorithm/algo.js
@@ -1,6 +1,7 @@
-const SolutionClass = class{
+const PrimeNumber = class{
     constructor(n){
         this.n = n;
+        this.primeNumberArray = this.createPrimeNumberArray();
     }
     createPrimeNumberArray(){
         const arr = [];
@@ -20,40 +21,53 @@ const SolutionClass = class{
         }
         return arr;
     }
-    solution(){
-        if(this.n%2===0){ // 혹시 인풋값에 짝수 있을 경우 처리
-            console.error('홀수를 입력해 주세요!');
-        }
-        if(this.n<=5){ // 5보다 작으면 만들 수 없음 처리
-            return 0;
-        }
-        const primeNumberArray = this.createPrimeNumberArray();
-        const resultArray = [];
-        for(let i=0; i<primeNumberArray.length; i++){ // 간단히 3중 for문 돌면서 찾으면 쉽게 끝난다.
-            if(resultArray.length>0){
-                break;
-            }
-            let sumI = primeNumberArray[i];
-            for(let j=0; j<primeNumberArray.length; j++){
-                if(sumI+primeNumberArray[j]>=this.n||resultArray.length>0){
-                    break;
-                }
-                let sumJ = sumI+primeNumberArray[j];
-                for(let k=0; k<primeNumberArray.length; k++){
-                    const sumK = sumJ+primeNumberArray[k];
-                    if(sumK>this.n||resultArray.length>0){
-                        break;
-                    }
-                    if(sumK===this.n){
-                        resultArray.push(primeNumberArray[i]);
-                        resultArray.push(primeNumberArray[j]);
-                        resultArray.push(primeNumberArray[k]);
-                    }
-                }
-            }
-        }
-        console.log(resultArray.join(' '));
+    getPrimeNumberArray(){
+        return this.primeNumberArray;
     }
 }
-const sc = new SolutionClass(2);
-sc.solution();
+
+//최악의 경우 loop수 :17265
+
+
+const solution = (n)=>{
+    if(n%2===0){ // 혹시 인풋값에 짝수 있을 경우 처리
+        console.error('홀수를 입력해 주세요!');
+    }
+    if(n<=5){ // 5보다 작으면 만들 수 없음 처리
+        return 0;
+    }
+    const primeNumber = new PrimeNumber(n);
+    const primeNumberArray = primeNumber.getPrimeNumberArray();
+    const resultArray = [];
+    // let max_loop = 0;
+    for(let i=0; i<primeNumberArray.length; i++){ // 간단히 3중 for문 돌면서 찾으면 쉽게 끝난다.
+        if(resultArray.length>0){
+            break;
+        }
+        // max_loop+=1;
+        let sumI = primeNumberArray[i];
+        for(let j=0; j<primeNumberArray.length; j++){
+            if(sumI+primeNumberArray[j]>=n||resultArray.length>0){
+                break;
+            }
+            // max_loop+=1;
+            let sumJ = sumI+primeNumberArray[j];
+            for(let k=0; k<primeNumberArray.length; k++){
+                const sumK = sumJ+primeNumberArray[k];
+                if(sumK>n||resultArray.length>0){
+                    break;
+                }
+                // max_loop+=1;
+                if(sumK===n){
+                    resultArray.push(primeNumberArray[i]);
+                    resultArray.push(primeNumberArray[j]);
+                    resultArray.push(primeNumberArray[k]);
+                }
+            }
+        }
+    }
+    console.log(resultArray.join(' '));
+    // console.log(max_loop); //최악의 경우
+};
+
+solution(999);
